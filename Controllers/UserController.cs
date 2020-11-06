dee7ac26ad6bdb1820a7383d3662574ef2c9814a
bhadresh gosai <45452903+bhadresh-goswami@users.noreply.github.com>
2020-11-06 12:17:43 -0500

    login page desing

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DTRS.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string emailid, string password)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+            return View();
+        }
+    }
+}
